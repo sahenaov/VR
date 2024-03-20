db930d904b71c980c70e6ba807f0dbd75589e2e3
sahenaov <sahenaov@unal.edu.co>
2024-03-20 15:35:16 -0500

    añadir zombie

--- Assets/Scripts/InicializarPosicion.cs
+++ Assets/Scripts/InicializarPosicion.cs
@@ -6,17 +6,16 @@ public class InicializarPosicion : MonoBehaviour
 {
     private void Start()
     {
-        Debug.Log("La vida es bonita");
-        //// Recuperar la posición y rotación de la cámara almacenada en PlayerPrefs
-        //float posX = PlayerPrefs.GetFloat("CamaraPosX");
-        //float posY = PlayerPrefs.GetFloat("CamaraPosY");
-        //float posZ = PlayerPrefs.GetFloat("CamaraPosZ");
-        //float rotX = PlayerPrefs.GetFloat("CamaraRotX");
-        //float rotY = PlayerPrefs.GetFloat("CamaraRotY");
-        //float rotZ = PlayerPrefs.GetFloat("CamaraRotZ");
+        // Recuperar la posición y rotación de la cámara almacenada en PlayerPrefs
+        float posX = PlayerPrefs.GetFloat("CamaraPosX");
+        float posY = PlayerPrefs.GetFloat("CamaraPosY");
+        float posZ = PlayerPrefs.GetFloat("CamaraPosZ");
+        float rotX = PlayerPrefs.GetFloat("CamaraRotX");
+        float rotY = PlayerPrefs.GetFloat("CamaraRotY");
+        float rotZ = PlayerPrefs.GetFloat("CamaraRotZ");
 
-        //// Asignar la posición y rotación a la cámara
-        //transform.position = new Vector3(posX, posY, posZ);
-        //transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
+        // Asignar la posición y rotación a la cámara
+        transform.position = new Vector3(posX, posY, posZ);
+        transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
     }
 }
